40cc62292ef9f2e700ab254e61fc7a95e84be628
Ahmedwafy <ahmedwafyasu@gmail.com>
2025-02-04 02:32:20 +0200

    fix clerk

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,8 @@ const nextConfig: NextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
+    NEXT_PUBLIC_CLERK_FRONTEND_API:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
 };
 
